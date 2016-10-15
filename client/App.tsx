4eab7c39702707d03e5ed8af81c320643ff980bb
Cameron Little <cameron@camlittle.com>
2016-10-15 13:43:28 -0700

    Added stuff to make start up time faster + manual reimport of friends

--- client/App.tsx
+++ client/App.tsx
@@ -14,6 +14,9 @@ import Game from "./components/Game";
 
 (window as any).React = React;
 
+const userInfo = {
+    fields: "name,email,picture.width(400).height(400)",
+};
 
 interface AppProps {
     fatalError?: FatalError;
@@ -79,6 +82,8 @@ export default class App extends React.Component<AppProps, AppState> {
         FB.getLoginStatus(response => {
             if (response.status === "connected") {
                 this.handleAuth(response.authResponse!);
+            } else {
+                this.setState({ loading: false });
             }
         });
     }
@@ -92,6 +97,7 @@ export default class App extends React.Component<AppProps, AppState> {
             });
             this.handleLoggedOut();
         }
+        this.setState({ loading: false });
         console.error(error);
     }
 
@@ -103,25 +109,33 @@ export default class App extends React.Component<AppProps, AppState> {
             friends: this.state.friends,
             friendsInApp: this.state.friendsInApp ? this.state.friendsInApp.map((f) => f.id) : undefined,
         } as InitializationMessage);
+        this.socket.on("connect", this.initServerConnection);
     }
 
     @autobind
-    collectFriends(response: any) {
-        if (response.error) this.handleFBError(response.error);
-        this.setState({
-            friends: (this.state.friends || []).concat(response.data as UserInfo[])
+    collectFriends(response: any): Promise<{}> {
+        return new Promise((resolve, reject) => {
+            if (response.error) {
+                reject({
+                    type: "facebook",
+                    error: response.error,
+                });
+                return;
+            } else {
+                this.setState({
+                    friends: (this.state.friends || []).concat(response.data as UserInfo[])
+                });
+                if (response.paging.next) {
+                    resolve(fetch(response.paging.next, {
+                        method: "get",
+                    })
+                    .then((response: any) => response.json())
+                    .then(this.collectFriends));
+                } else {
+                    resolve();
+                }
+            }
         });
-        if (response.paging.next) {
-            fetch(response.paging.next, {
-                method: "get",
-            })
-            .then((response: any) => response.json())
-            .then(this.collectFriends);
-        } else {
-            this.initServerConnection();
-            this.setState({ gettingFriends: false });
-            this.socket.on("connect", this.initServerConnection);
-        }
     }
 
     @autobind
@@ -131,26 +145,68 @@ export default class App extends React.Component<AppProps, AppState> {
             eventAction: "authed",
         });
         this.setState({ token: response.accessToken });
-        const userInfo = {
-            fields: "name,email,picture.width(400).height(400)",
-        };
         FB.api("/me", userInfo, (response) => {
             if (response.error) this.handleFBError(response.error);
             const r: UserInfo = response;
-            this.setState({ user: r });
+            this.setState({ user: r, loading: false });
             Bugsnag.user = {
                 id: r.id,
                 name: r.name,
                 email: r.email,
             };
 
-            // TODO use promises to make more async
-            FB.api(`${this.state.user!.id}/friends`, userInfo, (response) => {
-                if (response.error) this.handleFBError(response.error);
-                this.setState({ friendsInApp: response.data as UserInfo[] });
-                this.setState({ gettingFriends: true });
-                FB.api(`/${this.state.user!.id}/taggable_friends`, userInfo, this.collectFriends);
+            this.refreshAllFriends().then((res) => {
+                this.initServerConnection();
+            }).catch((err) => {
+                if ((err.type && err.type === "facebook")) {
+                    this.handleFBError(err.error);
+                }
             });
+        });
+    }
+
+    @autobind
+    refreshAllFriends(): Promise<{}> {
+        return Promise.all([ this.refreshPlayableFriends(), this.refreshTaggableFriends() ]);
+    }
+
+    refreshPlayableFriends(): Promise<{}> {
+        return new Promise((resolve, reject) => {
+            if (this.state.user) {
+                // TODO use promises to make more async
+                FB.api(`${this.state.user!.id}/friends`, this.state.user, (response) => {
+                    if (response.error) {
+                        reject({
+                            type: "facebook",
+                            error: response.error,
+                        });
+                    } else {
+                        this.setState({ friendsInApp: response.data as UserInfo[] });
+                        resolve();
+                    }
+                });
+            } else {
+                reject("user not found");
+            }
+        });
+    }
+
+    refreshTaggableFriends(): Promise<{}> {
+        return new Promise((resolve, reject) => {
+            if (this.state.user) {
+                this.setState({ gettingFriends: true });
+                FB.api(`/${this.state.user!.id}/taggable_friends`, userInfo, (response) => {
+                    resolve(this.collectFriends(response));
+                });
+            } else {
+                reject("user not found");
+            }
+        }).then((res) => {
+            this.setState({ gettingFriends: false });
+            return res;
+        }, (err) => {
+            this.setState({ gettingFriends: false });
+            throw err;
         });
     }
 
@@ -173,7 +229,6 @@ export default class App extends React.Component<AppProps, AppState> {
             ga("send", "event", {
                 eventCategory: "game",
                 eventAction: "start",
-                eventValue: friend.id,
             });
             return () => {
                 this.socket.emit("start", {
@@ -195,6 +250,10 @@ export default class App extends React.Component<AppProps, AppState> {
                 </div>
             );
         } else if (this.state.loading) {
+            Bugsnag.context = "Loading";
+            ga("send", "screenview", {screenName: "Loading"});
+            return <p>Loading...</p>;
+        } else {
             if (this.state.user) {
                 if (this.state.activeGame) {
                     Bugsnag.context = "Landing";
@@ -204,13 +263,17 @@ export default class App extends React.Component<AppProps, AppState> {
                     ga("send", "screenview", {screenName: "Game"});
                 }
                 return (
-                    <div>
+                    <div className="authed-app">
                         <div className="user-controls">
+                            <button
+                                className="refresh-button"
+                                onClick={this.refreshAllFriends}
+                                disabled={!!(this.state.gettingFriends || this.state.activeGame)}
+                            >Reimport</button>
                             <Logout onLoggedOut={this.handleLoggedOut} />
                             {this.state.user && <p>{this.state.user.name}</p>}
                         </div>
                         <h1>Guess Who‽</h1>
-                        {this.state.gettingFriends && <p>Finding your friends...</p>}
                         {this.state.activeGame
                             ? (
                                 <Game
@@ -218,18 +281,14 @@ export default class App extends React.Component<AppProps, AppState> {
                                     game={this.state.activeGame}
                                     socket={this.socket}
                                 />
-                            )
-                            : (
-                                <div>
-                                    {this.state.friendsInApp ? (
-                                        <Landing
-                                            friends={this.state.friendsInApp}
-                                            friendsOnline={this.state.onlineStatus!}
-                                            setUpGame={this.setUpWith}
-                                        />
-                                    ) : null}
-                                </div>
-                            )}
+                            ) : (
+                                this.state.friendsInApp ? (
+                                    <Landing
+                                        friends={this.state.friendsInApp}
+                                        friendsOnline={this.state.onlineStatus!}
+                                        setUpGame={this.setUpWith}
+                                    />
+                                ) : null)}
                     </div>
                 );
             } else {
@@ -237,10 +296,6 @@ export default class App extends React.Component<AppProps, AppState> {
                 ga("send", "screenview", {screenName: "Login"});
                 return <Login onLoggedIn={this.handleAuth} />;
             }
-        } else {
-            Bugsnag.context = "Loading";
-            ga("send", "screenview", {screenName: "Loading"});
-            return <p>Loading...</p>;
         }
     }
 }
--- client/components/Landing.tsx
+++ client/components/Landing.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import * as FB from "FB";
 
 import "./Landing.css";
 
+import FB from "../facebook";
 import FriendStatus from "./FriendStatus";
 
 
@@ -13,11 +13,22 @@ interface LandingProps {
 }
 
 export default function Landing(props: LandingProps) {
+    function inviteFriends(ev: React.MouseEvent) {
+        FB.ui({
+            method: "apprequests",
+            message: "Play with me!",
+            title: "Invite Friends",
+            filters: ["app_non_users"],
+        }, (response: any) => {});
+        ev.preventDefault();
+    }
+
     return (
         <div className="landing">
+            <button className="invite-button" onClick={inviteFriends}>Invite</button>
             <h3>Friends to play with</h3>
             {props.friends.length === 0
-                ? <p>You have no friends using this app 😢. Invite some!</p>
+                ? <p>You have no friends using this app 😢. <a href="#invite" onClick={inviteFriends}>Invite some!</a></p>
                 : <ul className="friends">{props.friends.map((friend) => (
                     <FriendStatus
                         key={friend.id}
