95b66e87c385f04d0fd1287a4f71a78b761f2698
Cameron Little <cameron@camlittle.com>
2016-10-16 11:26:51 -0700

    Added zoom functionality on tiles

--- client/components/Game/Tile.tsx
+++ client/components/Game/Tile.tsx
@@ -16,12 +16,74 @@ interface TileProps {
 interface TileState {
 }
 
+interface WithClientCoords {
+    clientY: number;
+    clientX: number;
+}
+
+function getWindowSize() {
+    return {
+        width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+    };
+}
+
 export default class Tile extends React.Component<TileProps, TileState> {
+    private trackedTouch: number | null = null;
+    private previousTransform: string | null = null;
+    private zoomTimeout: number;
+
+    private onScrubStart(el: HTMLElement) {
+        el.addEventListener("transitionend", this.removeTransition);
+    }
+
+    private onScrubEnd(el: HTMLElement) {
+        el.removeEventListener("transitionend", this.removeTransition);
+        el.style.transform = null;
+        el.style.transition = null;
+    }
+
+    private onScrubMove(el: HTMLElement, ev: WithClientCoords) {
+        if (this.previousTransform === null) {
+            const rect = el.getBoundingClientRect();
+            const xDeg = (((ev.clientY - rect.top) / rect.height) - .5) * 20;
+            const yDeg = (((ev.clientX - rect.left) / rect.width) - .5) * 20;
+            el.style.transform = `translateZ(10px) rotateY(${-yDeg}deg) rotateX(${xDeg}deg)`;
+        }
+    }
+
+    private delayStartZoom(el: HTMLElement) {
+        this.zoomTimeout = window.setTimeout(() => this.startZoom(el), 400);
+    }
+
+    private startZoom(el: HTMLElement) {
+        el.classList.add("game-tile-zoomed");
+        this.previousTransform = el.style.transform;
+        const rect = el.getBoundingClientRect();
+        const rectMid = rect.left + rect.width / 2;
+        const wSize = getWindowSize();
+        const moveX = (wSize.width / 2) - rectMid;
+        console.log(rectMid, wSize.width, moveX);
+        el.style.transition = null;
+        el.style.transform = `scale(3) translateZ(30px) translateX(${moveX / 3}px)`;
+        el.style.zIndex = `100`;
+        el.removeEventListener("transitionend", this.removeZIndex);
+    }
+
+    private stopZoom(el: HTMLElement) {
+        window.clearTimeout(this.zoomTimeout);
+        el.classList.remove("game-tile-zoomed");
+        el.style.transition = null;
+        el.style.transform = this.previousTransform;
+        this.previousTransform = null;
+        el.addEventListener("transitionend", this.removeZIndex);
+    }
 
     @autobind
-    private onScrubStart(ev: React.MouseEvent) {
+    private removeZIndex(ev: Event) {
         const el = (ev.currentTarget as HTMLElement);
-        el.addEventListener("transitionend", this.removeTransition);
+        el.style.zIndex = null;
+        el.removeEventListener("transitionend", this.removeZIndex);
     }
 
     @autobind
@@ -32,21 +94,65 @@ export default class Tile extends React.Component<TileProps, TileState> {
     }
 
     @autobind
-    private onScrubEnd(ev: React.MouseEvent) {
-        const el = (ev.currentTarget as HTMLElement);
-        el.classList.remove("active");
-        el.style.transform = null;
-        el.style.transition = null;
-        el.removeEventListener("transitionend", this.removeTransition);
+    private onMouseMove(ev: React.MouseEvent) {
+        this.onScrubMove(ev.currentTarget as HTMLElement, ev);
+        window.clearTimeout(this.zoomTimeout);
     }
 
     @autobind
-    private onScrubMove(ev: React.MouseEvent) {
-        const el = (ev.currentTarget as HTMLElement);
-        const rect = el.getBoundingClientRect();
-        const xDeg = (((ev.clientY - rect.top) / rect.height) - .5) * 20;
-        const yDeg = (((ev.clientX - rect.left) / rect.width) - .5) * 20;
-        el.style.transform = `translateZ(10px) rotateY(${-yDeg}deg) rotateX(${xDeg}deg)`;
+    private onMouseOver(ev: React.MouseEvent) {
+        this.onScrubStart(ev.currentTarget as HTMLElement);
+    }
+
+    @autobind
+    private onMouseOut(ev: React.MouseEvent) {
+        this.onScrubEnd(ev.currentTarget as HTMLElement);
+        this.stopZoom(ev.currentTarget as HTMLElement);
+    }
+
+    @autobind
+    private onTouchMove(ev: React.TouchEvent) {
+        if (this.trackedTouch !== null) {
+            const touch = Array.from(ev.changedTouches).find((t) => t.identifier === this.trackedTouch);
+            if (touch) {
+                this.onScrubMove(ev.currentTarget as HTMLElement, touch);
+                if (!this.previousTransform) {
+                    // ev.preventDefault();
+                }
+            }
+        }
+    }
+
+    @autobind
+    private onTouchStart(ev: React.TouchEvent) {
+        if (this.trackedTouch === null) {
+            this.trackedTouch = ev.changedTouches.item(0).identifier;
+            this.onScrubStart(ev.currentTarget as HTMLElement);
+            this.delayStartZoom(ev.currentTarget as HTMLElement);
+        }
+    }
+
+    @autobind
+    private onTouchEnd(ev: React.TouchEvent) {
+        if (this.trackedTouch !== null) {
+            const touch = Array.from(ev.changedTouches).find((t) => t.identifier === this.trackedTouch);
+            if (touch) {
+                this.trackedTouch = null;
+                this.onScrubEnd(ev.currentTarget as HTMLElement);
+                this.stopZoom(ev.currentTarget as HTMLElement);
+            }
+        }
+    }
+
+    @autobind
+    private onMouseUp(ev: React.MouseEvent) {
+        this.stopZoom(ev.currentTarget as HTMLElement);
+    }
+
+    @autobind
+    private onMouseDown(ev: React.MouseEvent) {
+        this.delayStartZoom(ev.currentTarget as HTMLElement);
+        ev.preventDefault(); // prevent text selection
     }
 
     render() {
@@ -69,12 +175,15 @@ export default class Tile extends React.Component<TileProps, TileState> {
             <div
                 className={classes}
                 onClick={this.props.onClick}
-                onMouseMove={this.onScrubMove}
-                onMouseOver={this.onScrubStart}
-                onMouseOut={this.onScrubEnd}
-                // onTouchMove={this.onScrubMove}
-                // onTouchStart={this.onScrubStart}
-                // onTouchEnd={this.onScrubEnd}
+                onMouseMove={this.onMouseMove}
+                onMouseOver={this.onMouseOver}
+                onMouseOut={this.onMouseOut}
+                onMouseDown={this.onMouseDown}
+                onMouseUp={this.onMouseUp}
+                onTouchMove={this.onTouchMove}
+                onTouchStart={this.onTouchStart}
+                onTouchEnd={this.onTouchEnd}
+                onTouchCancel={this.onTouchEnd}
                 tabIndex={this.props.eliminated ? -1 : 0}
             >
                 <div className="game-tile-contents" style={{
